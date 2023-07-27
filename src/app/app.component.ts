ee548f0bd97fa806412af720a0ec9709910d546b
imskanand <the.anand3099@gmail.com>
2023-07-27 21:08:46 +0530

    Initialized the starter project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, isDevMode } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  ngOnInit(): void {
+    if(isDevMode()){
+      console.log("Development Mode!");
+    }else{
+      console.log("Production Mode..")
+    }
+  }
   title = 'task-management';
 }
